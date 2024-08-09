f4c3b583f1e4c80447aa9f0cdf16a9d52ea796e3
mingminQ <149143017+mingminQ@users.noreply.github.com>
2024-08-10 01:56:45 +0900

    Update pure_pursuit.cpp

--- pure_pursuit.cpp
+++ pure_pursuit.cpp
@@ -1,76 +1,86 @@
 #include <cmath>
 #include <vector>
 
+struct Pose2D
+{
+  double x, y, yaw;
+};
+
+struct cmd_vel
+{
+  double v, w;
+};
+
 class controller_purepursuit
 {
-  private: // menber variables for pure pursuit controller
-  std::vector<float32> waypoints;
-  double LookAheadDistance;
-  double maxSteering;
-  double wheelBase;
-  int currentIdx;
-
-  double target_x;
-  double target_y;
-
-  double current_x;
-  double current_y;
-  double current_yaw;
-
-  double targetDistance; 
-
-  int x   = 0;
-  int y   = 1;
-  int yaw = 2;
-
   public:
+
+  // initialize controller
   void init_controller()
   {
   
   }
 
-  void find_target_point(std::vector<float32> vehiclePose)
+  // calculate distance between two points
+  double get_distance( Pose2D point1, Pose2D point2 )
   {
-    // set current vehicle position to rear wheel
-    current_x   = vehiclePose[x] - wheelBase * cos(vehiclePose[yaw]);
-    current_y   = vehiclePose[y] - wheelBase * sin(vehiclePose[yaw]);
-    current_yaw = vehiclePose[yaw];
+    return sqrt( pow(point2.x - point1.x, 2) + pow(point2.y - point1.y) );
+  }
 
-    // find target point satisfying condition
+  // calculate rear vehicle position
+  Pose2D get_vehicle_position_rear( Pose2D vehiclePose )
+  {
+    Pose2D vehiclePose_raer;
+    vehiclePose_raer.x   = vehiclePose.x - ( wheelBase / 2.0 ) * cos( vehiclePose.yaw );
+    vehiclePose_raer.x   = vehiclePose.y - ( wheelBase / 2.0 ) * sin( vehiclePose.yaw );
+    vehiclePose_rear.yaw = vehiclePose.yaw;
+  }
+
+  // calculate target waypoint
+  Pose2D get_target_point( Pose2D vehiclePose )
+  {
+      Pose2D targetPoint;
+    
+      if( currentIdx == waypoints.size() )
+      {
+        targetPoint.x = waypoints[ waypoints.size() - 1 ].x;
+        targetPoint.y = waypoints[ waypoints.size() - 1 ].y;
+        return targetPoint;
+      }
+
+    Pose2D vehiclePose_raer = get_vehicle_position_rear( vehiclePose );
+
+
     for(size_t idx = currentIdx; idx < waypoints.size(); idx++)
     {
-      targetDistance = sqrt( pow(waypoint[idx][x] - current_x, 2) + pow(waypoint[idx][y] - current_y, 2));
-
-      if(LookAheadDistance < targetDistance)
+      double distance = get_distance( vehiclePose_raer, waypoints[idx] );
+      
+      if( LookAheadDistance <= distance )
       {
-        target_x = waypoint[idx][x];
-        target_y = waypoint[idx][y];
+        targetPoint.x = waypoints[idx].x;
+        targetPoint.y = waypoints[idx].y;
         currentIdx = idx;
-        return;
+        return targetPoint;
       }
     }
-
-    // faild to find target point satisfying condition
-    if(currentIdx == waypoint.size())
-    {
-      target_x = waypoint.end()[x];
-      target_y = waypoint.end()[y];
-    }
   }
 
-  double calculate_steering()
+  // calculate steering angle
+  Pose2D get_steering_angle( Pose2D vehiclePose )
   {
-  
-  }
-
-  std::vector<float32> control(std::vector<float32> vehiclePose)
-  {
-    find_target_point(vehiclePose);
+    Pose2D targetPoint = get_target_point(vehiclePose);
     
-    double steering = calculate_steering(vehiclePose);
-    steering = maxSteering < steering ? maxSteering : ( steering < -maxSteering ? -maxSteering : steering);
+  }
 
-    std::vector<float> cmd_vel = {linearVelocity, steering};
-    return cmd_vel;
+  // return linear velocity and steering angle
+  cme_vel control( Pose2D vehiclePose)
+  {
+    cmd_vel control_input;
+    control_input.w = get_steering_angle( vehiclePose );
+    
+    // add linear velocity controller
+    // control_input.v = 
+    
+    return control_input;
   }
 };
